ad99051426456c7f94c4e8a6bf8405ccbca36e25
Supriya <supriya.yerramilli@gmail.com>
2018-05-02 01:21:30 -0400

    need to fix error on zipcode page

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import ZipForm from './ZipForm';
 
 class App extends React.Component {
+    constructor(props) {
+    super(props);
+
+    this.state= {
+        zipcode: ''
+    };
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    
+}
+    onFormSubmit(zipcode){
+        this.setState({zipcode});
+    }
     render() {
         return (
         <div className="app">
-            <p>Scotch school rocks!</p>
+        <ZipForm onSubmit={this.onFormSubmit}/>
         </div>
         );
     }
